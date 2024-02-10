60923fe1f05f82ed586900986456b02bb6fb8cbb
LWCoding <DiamondMob01@gmail.com>
2024-02-10 13:33:53 -0800

    Fixed typos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ function App() {
       />
       <main>
         <a target="#" href="https://discord.gg/DdS4ZwQUW9" class="call-to-action">Join Us</a>
-        <ContentBlock header="What We Do" text="We organize meetups with other furries in the area, make crafts, draw art together, and go to conventions. We have regular meeWe have a community of Stanfurs and organize events with other furries in the Bay Area." />
+        <ContentBlock header="What We Do" text="We organize meetups with other furries in the area, make crafts, draw art together, and go to conventions. We have a community of Stanfurs and organize events with other furries in the Bay Area." />
         <ContentBlock />
         <ContentBlock header="How do I get involved?" text="We're most active on Discord. Alternatively, if you want to meet more furries in the area, join our Telegram group. You don't have to be a furry to join–if you want to see what it's all about, stop by. We guarantee you'll have a paw-some time :3" />
         <ContentBlock />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer>
-      Questions? Concerns? Get in touch with us by 
+      Questions? Concerns? Get in touch with us by{" "}
       <a href="mailto:furries.at.stanford@gmail.com">email</a>.
     </footer>
   );
