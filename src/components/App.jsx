3d1645b1a92c22c9b7d19703e5e24d33bba222fa
AlurienFlame <62194841+AlurienFlame@users.noreply.github.com>
2024-02-12 09:42:03 -0800

    Slight button gradient

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,12 +15,12 @@ function App() {
         alt="Furries at Stanford logo"
       />
       <main>
-        <a target="#" href="https://discord.gg/DdS4ZwQUW9" class="call-to-action">Join Us</a>
+        <a target="#" href="https://discord.gg/DdS4ZwQUW9" className="call-to-action">Join Us</a>
         <ContentBlock header="What We Do" text="We organize meetups with other furries in the area, make crafts, draw art together, and go to conventions. We have a community of Stanfurs and organize events with other furries in the Bay Area." />
         <ContentBlock />
         <ContentBlock header="How do I get involved?" text="We're most active on Discord. Alternatively, if you want to meet more furries in the area, join our Telegram group. You don't have to be a furry to join–if you want to see what it's all about, stop by. We guarantee you'll have a paw-some time :3" />
         <ContentBlock />
-        <a target="#" href="https://discord.gg/DdS4ZwQUW9" class="call-to-action">Join Our Discord</a>
+        <a target="#" href="https://discord.gg/DdS4ZwQUW9" className="call-to-action">Join Our Discord</a>
       </main>
       <Footer />
     </div>
