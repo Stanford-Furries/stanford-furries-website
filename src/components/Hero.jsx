980d6abc55fd469c5e222f701f57587153b5abd9
LWCoding <DiamondMob01@gmail.com>
2024-02-16 12:36:53 -0800

    *unnests your css* :3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,19 @@
 import "./Hero.css";
 
 function Hero(props) {
-    return (
-        <div id="hero-container">
-            <img id="hero-img" src={props.src} alt={props.alt} />
-            <div id="hero-overlay">
-                <h1 className="animate__animated animate__fadeInDown">{props.title}</h1>
-                <p className="animate__animated animate__fadeInDown animate__delay-1s">{props.subtext}</p>
-            </div>
-        </div>
-    );
+	return (
+		<div className="hero-container">
+			<img className="hero-img" src={props.src} alt={props.alt} />
+			<div className="hero-overlay">
+				<h1 className="animate__animated animate__fadeInDown">
+					{props.title}
+				</h1>
+				<p className="animate__animated animate__fadeInDown animate__delay-1s">
+					{props.subtext}
+				</p>
+			</div>
+		</div>
+	);
 }
 
 export default Hero;
